4c43f53a7f502b85840091d03a0ff1edfcc16769
II9tka <51323231+II9tka@users.noreply.github.com>
2021-08-20 15:39:29 +0300

    Added pagination to user favorites

--- backend/garpix_favourite/views.py
+++ backend/garpix_favourite/views.py
@@ -25,9 +25,12 @@ class FavoriteViewSet(mixins.CreateModelMixin,
 
     @action(methods=['GET'], detail=False, permission_classes=(IsAuthenticated,), url_path='current')
     def get_user_favorites(self, request):
-        data = Favorite.objects.filter(user=request.user)
-        serializer = self.get_serializer(data, many=True)
+        queryset = self.filter_queryset(self.get_queryset())
 
-        return Response(
-            serializer.data, status=status.HTTP_200_OK
-        )
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
