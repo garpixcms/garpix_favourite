386653384b08c022a33e8e4dd85111d416c93a6d
griviala <78097561+griviala@users.noreply.github.com>
2023-05-10 19:12:41 +0300

    2.0.0

--- backend/garpix_favourite/setup.py
+++ backend/garpix_favourite/setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 from os import path
-from m2r import convert
-from django.conf import settings
 
+here = path.join(path.abspath(path.dirname(__file__)), 'garpix_favourite')
 
-with open(path.join(settings.BASE_DIR, '..', 'README.md'), encoding='utf-8') as f:
-    long_description = convert(f.read())
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='garpix_favourite',
     version='2.0.0',
     description='',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/garpixcms/garpix_favourite',
     author='Garpix LTD',
     author_email='info@garpix.com',
