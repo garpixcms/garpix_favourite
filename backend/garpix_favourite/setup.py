f70555053e129804f19cdd5a03c706922bbb14a1
griviala <78097561+griviala@users.noreply.github.com>
2023-05-22 11:13:34 +0300

    2.1.0

--- backend/garpix_favourite/setup.py
+++ backend/garpix_favourite/setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='garpix_favourite',
-    version='2.0.0',
+    version='2.1.0',
     description='',
     long_description=long_description,
     long_description_content_type='text/markdown',
